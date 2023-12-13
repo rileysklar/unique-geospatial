e2f27948f9fd283dd50de149ba1b1cd29e4a865a
Riley Sklar <riley.sklar@gmail.com>
2023-12-12 21:15:48 -0600

    first push

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Unique Geospatial";
+export const SITE_DESCRIPTION =
+  "UGS is the leading provider of geospatial solutions, offering expertise in GIS, CAD, UAVs, GPS, remote sensing, LiDAR, surveying, spatial analysis, and more.";
